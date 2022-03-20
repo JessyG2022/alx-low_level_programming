462e2cb9261d1ecabc30358e9a7b7f4462182b56
JessyG2022 <99347424+JessyG2022@users.noreply.github.com>
2022-03-20 10:17:26 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * m hello. printf task
+ * my hello. putc task
  * Return: return 1 after program execution
- *\
+ */
 int main(void)
 {
 putc("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
